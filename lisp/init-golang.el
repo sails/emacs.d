0c2411cdfee62f937a38a80fa3a9ad4040243dad
sailsxu <sailsxu@tencent.com>
2020-02-16 22:25:43 +0800

    golang支持远程编辑

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -5,11 +5,16 @@
 
 (require-package 'go-mode)
 (require-package 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp)
 (add-hook 'go-mode-hook
           (lambda ()
 
                                         ; Use goimports instead of go-fmt
             (setq gofmt-command "goimports")
+            ;; goimports在tramp模式下支持不好，它对目录结构有要求,而tramp时是在
+            ;; 临时目录下，所以在编辑远程文件时，不能使用goimports
+            ;; (setq gofmt-command "gofmt")
+
             ;; Call Gofmt before saving
             (add-hook 'before-save-hook 'gofmt-before-save)
                                         ; Godef jump key binding
@@ -26,18 +31,13 @@
 
             (setq-default flycheck-disabled-checkers '(go-build go-vet))
 
-
-
-            ))
-
-(add-hook 'go-mode-hook #'lsp)
-(with-eval-after-load 'lsp-mode (lsp-register-client
-                                 (make-lsp-client
-                                  :new-connection (lsp-tramp-connection "/data/sailsxu/go_proj/bin/gopls")
-                                  :major-modes '(go-mode)
-                                  :remote? t
-                                  :server-id 'go-remote)))
-
+            (require-package 'lsp-mode)
+            (with-eval-after-load 'lsp-mode (lsp-register-client
+                                             (make-lsp-client
+                                              :new-connection (lsp-tramp-connection "/data/sailsxu/go_proj/bin/gopls")
+                                              :major-modes '(go-mode)
+                                              :remote? t
+                                              :server-id 'go-remote)))))
 
 (provide 'init-golang)
 ;;; init-golang.el ends here
---
+++ lisp/init-tramp.el
@@ -0,0 +1,30 @@
+;;; init-tramp.el ---  tramp -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'tramp)
+
+;; /sshx:devnet:~/
+(setq tramp-default-method "sshx")
+(setq tramp-chunksize 2000)
+(tramp-set-completion-function "sshx"
+                               '((tramp-parse-sconfig "/etc/ssh_config")
+                                 (tramp-parse-sconfig "~/.ssh/config")))
+
+;; 加快tramp打开文件速度
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+              vc-ignore-dir-regexp
+              tramp-file-name-regexp))
+(setq tramp-verbose 1)
+
+(with-eval-after-load 'tramp
+  (message "on tramp")
+  (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+  (add-to-list 'tramp-remote-path '("/data/sailsxu/vas_go_proj/bin" "/data/sailsxu/go_proj/bin" "/usr/local/go/bin"))
+  )
+
+
+(provide 'init-tramp)
+;;; init-tramp.el ends here
